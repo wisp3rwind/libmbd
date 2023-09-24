c0cdadc4bb6f7b839bb0964be0b637ce87190026
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2023-09-24 09:02:41 +0200

    debug: add -fstack-usage

--- cmake/fortran_flags_override.cmake
+++ cmake/fortran_flags_override.cmake
@@ -1,6 +1,6 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_Fortran_FLAGS_INIT
-        "-fall-intrinsics -std=f2008ts -pedantic -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -Wno-maybe-uninitialized")
+        "-fall-intrinsics -std=f2008ts -pedantic -Wall -Wcharacter-truncation -Wimplicit-procedure -Wextra -Wno-maybe-uninitialized -fstack-usage")
     set(CMAKE_Fortran_FLAGS_DEBUG_INIT
-        "-Og -fcheck=all -fno-check-array-temporaries")
+        "-Og -fcheck=all -fno-check-array-temporaries -fstack-usage")
 endif()
